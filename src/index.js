0c09e2bba8d62d6243c051c99d4c8236eb483f1d
unknown <ekpenisiraphael@gmail.com>
2022-10-06 14:53:39 +0530

    Array object added with index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,65 @@
 import './style.css';
-import { addList, displayList } from './modules/appbuilder.js';
-import localGet from './modules/localstorage';
+// import { addList, displayList,} from './modules/appbuilder.js';
+// import localGet from './modules/localstorage';
 
-const addBtn = document.querySelector('.add');
-const task = document.querySelector('.task');
+// const addBtn = document.querySelector('.add');
+// const task = document.querySelector('.task');
 
-window.addEventListener('load', () => {
-  displayList();
-});
+// window.addEventListener('load', () => {
+//   displayList();
+// });
 
-addBtn.addEventListener('click', () => {
-  addList(task.value, false, localGet.length + 1);
+// addBtn.addEventListener('click', () => {
+//   addList(task.value, false, localGet.length + 1);
+// });
+
+const toDoList = [
+  {
+    description: 'dishes',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'study',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Bath',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'movies',
+    completed: true,
+    index: 4,
+  },
+];
+
+toDoList.forEach((e) => {
+  const { description, completed, index } = e;
+
+  const ulLists = document.querySelector('.ul-items');
+
+  const list = document.createElement('li');
+  list.setAttribute('class', 'list');
+  list.setAttribute('id', index);
+  ulLists.appendChild(list);
+
+  const span = document.createElement('span');
+  span.setAttribute('class', 'list-check');
+  list.appendChild(span);
+
+  const checkbox = document.createElement('input');
+  checkbox.setAttribute('type', 'checkbox');
+  checkbox.checked = completed;
+  span.appendChild(checkbox);
+
+  const paragraph = document.createElement('p');
+  paragraph.textContent = description;
+  span.appendChild(paragraph);
+
+  const ellipsis = document.createElement('i');
+  ellipsis.setAttribute('class', 'fa fa-ellipsis-v');
+  list.appendChild(ellipsis);
 });
